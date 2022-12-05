e88a7663d35a88c496dd74d79c8ed98e593d0c8a
maybo <mayboroda2045@gmail.com>
2022-12-06 01:01:59 +0200

    Добавьте файлы проекта.

---
+++ Maiboroda 3.3 Var 3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Майборода А.А. - Вар: 3
+
+var nValue = int.Parse(Console.ReadLine());
+var xValue = double.Parse(Console.ReadLine());
+
+double sum = 0;
+for (var iteration = 0; iteration < nValue; iteration++)
+{
+    var numberDegree = 1 + 2 * iteration;
+
+    double factorial = 1;
+    for (var number = 1; number <= numberDegree; number++) factorial *= number;
+
+    var temperance = Math.Pow(xValue, numberDegree) / factorial;
+
+    sum += iteration % 2 == 0 ? temperance : -temperance;
+}
+
+Console.WriteLine(sum);
